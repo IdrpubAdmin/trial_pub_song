af15a8855b878a3a45ff83f2d83693e2191ccdd9
undo <undo@idrsystems.co.kr>
2021-08-25 18:04:42 +0900

    swiper 사용

--- study/provence/js/common.js
+++ study/provence/js/common.js
@@ -1,6 +1,22 @@
-$('.post-wrapper').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
+    loop:true,
+    direction: getDirection(),
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    }
+    }
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
+
+    return direction;
+}
