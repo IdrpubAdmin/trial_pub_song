f5d59ba545e958345f06e894db491a4113b970df
undo <undo@idrsystems.co.kr>
2021-08-30 18:04:13 +0900

    걸프 수정중

--- study/provence/js/common.js
+++ study/provence/js/common.js
@@ -12,31 +12,36 @@ document.addEventListener('DOMContentLoaded',  () => {
     console.log(toggleBtns)
 
     toggleBtns.forEach(function(navItem,idx,list){
-        //console.log(navItem)
+        console.log(navItem)
 
         let navItem1d = navItem.querySelector('.nav-menu>li>a');
         let navItem2d = navItem.querySelector('.sub-menu');
+        let navItem3d = navItem.querySelector('.sub-menu>li>a');
 
        // console.log(navItem2d)
 
-        // 1depth에 마우스 오버시 2depth 노출
+        // 1depth에 마우스 오버시 메뉴 노출
         navItem.addEventListener('mouseover', function () {
             navItem2d.classList.add('active');
+            navItem1d.classList.add('on');
+            
         })
         navItem.addEventListener('mouseout', function () {
             navItem2d.classList.remove('active');
+            navItem1d.classList.remove('on');
         })
 
-        // 2depth에 마우스 오버시 1depth 배경색 변경
+        // 2depth에 마우스 오버시 폰트컬러 변경
         navItem2d.addEventListener('mouseover', function () {
-            navItem1d.classList.add('active_02');
+            navItem3d.classList.add('active_02');
         })
         navItem2d.addEventListener('mouseout', function () {
-            navItem1d.classList.remove('active_02');
+            navItem3d.classList.remove('active_02');
         })
     });
 
 })
+
 
 /*----------------------------------------------------------- */
 /* ready s -------------------------------------------------- */
