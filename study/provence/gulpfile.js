454089b70b5f45961093933dec38b121d10e7f9f
undo <undo@idrsystems.co.kr>
2021-09-02 17:18:27 +0900

    test1

--- study/provence/gulpfile.js
+++ study/provence/gulpfile.js
@@ -6,16 +6,16 @@ const fileinclude = require('gulp-file-include');
 
 // html파일 인클루드
 gulp.task('fileinclude', async function () {
-    gulp.src(['**/*.html'])
+    gulp.src(['index.html'])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
-        .pipe(gulp.dest('./page_include'));
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', function () {
-    gulp.watch('./css_dev/**/*.scss', gulp.series('sass')),
-    gulp.watch('**/*.html', gulp.series('fileinclude'));
-    return
-});
+// gulp.task('default', function () {
+//     gulp.watch('./css_dev/**/*.scss', gulp.series('sass')),
+//     gulp.watch('**/*.html', gulp.series('fileinclude'));
+//     return
+// });
