0cc37f06ab4ee6b47548afcc36f51e49ea89ee84
undo <undo@idrsystems.co.kr>
2021-11-22 18:07:41 +0900

    js test

--- study/lh_applyhome/js/common.js
+++ study/lh_applyhome/js/common.js
@@ -2,6 +2,32 @@
 
 window.addEventListener('DOMContentLoaded', function()
 {
+    const menu = document.getElementsByClassName("gnb_list");
+
+    function slideDown(){
+        menu.style.left ="-280px";
+    }
+
+    function slideUp(){
+        menu.style.left="0px";
+    }
+
+    function handleMouseMove(event){
+        let clientX = event.clientX;
+
+    if(clientX >=0 && clientX <=100){
+        slideUp();
+    }else if (clientX > 290) {
+        slideDown();
+    }
+    }
+
+    function init(){
+        document.addEventListener("mousemove", handleMouseMove);
+    }
+
+    init();
+
 
 });
 
