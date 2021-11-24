f3a09486d97b3a2af19a5a392dcab43cf213a175
undo <undo@idrsystems.co.kr>
2021-11-24 18:00:21 +0900

    js 작업중

--- study/lh_applyhome/js/common.js
+++ study/lh_applyhome/js/common.js
@@ -2,34 +2,39 @@
 
 window.addEventListener('DOMContentLoaded', function()
 {
-    const menu = document.getElementsByClassName("gnb_list");
+    const menu = document.querySelectorAll('.clcl');
+    const bg = document.querySelector('.gnb_bg');
 
-    function slideDown(){
-        menu.style.left ="-280px";
+    for (var i = 0; i < menu.length; i++) {
+        menu[i].addEventListener('mouseenter', function(){
+            for(var x = 0; x < menu.length; x++){
+                menu[x].classList.remove('on');
+            }
+            this.classList.add('on');
+        });
+        menu[i].addEventListener('mouseleave', function(){
+            for(var x = 0; x < menu.length; x++){
+                menu[x].classList.remove('on');
+            }
+
+        });
     }
 
-    function slideUp(){
-        menu.style.left="0px";
-    }
+    var nav = document.querySelector('.gnb_wrap');
 
-    function handleMouseMove(event){
-        let clientX = event.clientX;
-
-    if(clientX >=0 && clientX <=100){
-        slideUp();
-    }else if (clientX > 290) {
-        slideDown();
-    }
-    }
-
-    function init(){
-        document.addEventListener("mousemove", handleMouseMove);
-    }
-
-    init();
-
+    nav.addEventListener('mouseenter', function(){
+        bg.style.left = '280px';
+        /* bg.style.opacity = '1'; */
+        bg.style.zIndex = '1';
+        bg.style.display = 'block';
+    });
+    nav.addEventListener('mouseleave', function(){
+        bg.style.left = '0px';
+        /* bg.style.opacity = '0'; */
+        bg.style.zIndex = '-1';
+        bg.style.display = 'none';
+    });
 
 });
-
 
 
