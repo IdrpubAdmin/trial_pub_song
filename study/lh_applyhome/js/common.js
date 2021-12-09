f34e4eb5e5368c09ffc634ee3b345a5ecbb47a36
undo <undo@idrsystems.co.kr>
2021-12-09 18:05:21 +0900

    모바일 작업중

--- study/lh_applyhome/js/common.js
+++ study/lh_applyhome/js/common.js
@@ -1,7 +1,8 @@
 
 
-window.addEventListener('DOMContentLoaded', function()
-{
+(function() {
+    'use strict';  // 정의되어있지않는 문법을 썼을때 consol창에 에러뜨게해줌, 성능개선효과
+
     const menu = document.querySelectorAll('.menu');
     const bg = document.querySelector('.gnb_bg');
     const dim = document.querySelector('.dim');
@@ -19,8 +20,24 @@ window.addEventListener('DOMContentLoaded', function()
             }
 
         });
+    };
+
+    var nav = document.querySelector('.gnb_wrap');
+    nav.addEventListener('mouseenter', mouseenter);
+    nav.addEventListener('mouseleave', mouseleave);
+
+    function mouseenter() {
+        bg.classList.add('active');
+        dim.classList.add('_on');
     }
 
+    function mouseleave() {
+        bg.classList.remove('active');
+        dim.classList.remove('_on');
+    }
+
+    /*
+    위에처럼 변수화 시켜서 사용할수있다 (중복사용될 이벤트라면)
     var nav = document.querySelector('.gnb_wrap');
 
     nav.addEventListener('mouseenter', function(){
@@ -31,7 +48,8 @@ window.addEventListener('DOMContentLoaded', function()
         bg.classList.remove('active');
         dim.classList.remove('_on');
     });
+ */
 
-});
+})();
 
 
