83d12d2af888ca48afadec25cdb5a1637ca5b0b3
undo <undo@idrsystems.co.kr>
2021-12-22 18:03:32 +0900

    top버튼 위치수정

--- study/provence/js/common.js
+++ study/provence/js/common.js
@@ -5,31 +5,35 @@
 /* ========================================================== */
 /*----------------------------------------------------------- */
 
-// 메뉴 호버시 서브메뉴 드롭다운
+(function() {
+    'use strict';
 
-document.addEventListener('DOMContentLoaded',  () => {
-    const toggleBtns = document.querySelectorAll('.nav-menu>li');
+    // 메뉴 호버시 서브메뉴 드롭다운
+    document.addEventListener('DOMContentLoaded',  () => {
+        const toggleBtns = document.querySelectorAll('.nav-menu>li');
 
-    toggleBtns.forEach(function(navItem,idx,list){
+        toggleBtns.forEach(function(navItem,idx,list){
 
-        let navItem1d = navItem.querySelector('.nav-menu>li>a');
-        let navItem2d = navItem.querySelector('.sub-menu');
+            let navItem1d = navItem.querySelector('.nav-menu>li>a');
+            let navItem2d = navItem.querySelector('.sub-menu');
 
-        // 1depth에 마우스 오버시 메뉴 노출
-        navItem.addEventListener('mouseover', function () {
-            navItem2d.classList.add('active');
-            navItem1d.classList.add('on');
-            
-        })
-        navItem.addEventListener('mouseout', function () {
-            navItem2d.classList.remove('active');
-            navItem1d.classList.remove('on');
-        })
-    });
+            // 1depth에 마우스 오버시 메뉴 노출
+            navItem.addEventListener('mouseover', function () {
+                navItem2d.classList.add('active');
+                navItem1d.classList.add('on');
 
-})
+            })
+            navItem.addEventListener('mouseout', function () {
+                navItem2d.classList.remove('active');
+                navItem1d.classList.remove('on');
+            })
+        });
+
+    })
 
 
+})()
 
-/* ready e -------------------------------------------------- 
+
+/* ready e --------------------------------------------------
 /*----------------------------------------------------------- */
