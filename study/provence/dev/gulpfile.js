e165e582cb5043f9958c1b1976e2feaad10e1653
undo <undo@idrsystems.co.kr>
2021-12-28 18:02:09 +0900

    절대경로완료-include 축소함

--- study/provence/dev/gulpfile.js
+++ study/provence/dev/gulpfile.js
@@ -1,6 +1,8 @@
 
 const gulp = require('gulp');
 const fileinclude = require('gulp-file-include');
+/* const webserver = require('gulp-webserver');
+const connect =connect = require('gulp-connect'); */
 
 // html파일 인클루드
 gulp.task('fileinclude', async function () {
@@ -17,3 +19,25 @@ gulp.task('default', function () {
     gulp.watch('**/*.html', gulp.series('fileinclude'));
     return
 })
+
+
+// 실시간 반영
+// gulp.task('connect', function(){
+//     connect.server({
+//         root: '**/*.html',
+//         livereload: true,
+//         port: 8001
+//     });
+// });
+
+// gulp.task('html', function(){
+//     gulp.src('**/*.html')
+//     .pipe(connect.reload());
+// });
+
+// gulp.task('watch', function(){
+//     gulp.watch(['**/*.html'], ['html']); // html 리로드
+// });
+
+// gulp.task('default', gulp.parallel('connect', 'watch'));
+
