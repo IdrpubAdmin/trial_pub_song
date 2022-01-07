0d710215b9f8324204eed2ee665de0f73f6ebda3
undo <undo@idrsystems.co.kr>
2022-01-07 18:02:47 +0900

    gulp setting

--- study/provence/dev/gulpfile.js
+++ study/provence/dev/gulpfile.js
@@ -2,6 +2,7 @@
 const gulp = require('gulp');
 const fileinclude = require('gulp-file-include');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
 
 
 // html파일 인클루드
@@ -15,52 +16,45 @@ gulp.src(['**/*.html'])
 });
 
 // 파일 변경 감지
-gulp.task('default', function () {
+gulp.task('include:watch', function () {
     gulp.watch('**/*.html', gulp.series('fileinclude'));
     return
 })
 
 
-
 //liveserver
-
 gulp.task('browserSync', function() {
     browserSync.init({
-    server: {
-        baseDir: "../dist/page", // 서버에 띄울 폴더 위치 지정
-        directory: true
-    }
+        server: {
+            baseDir: "../dist/page", // 서버에 띄울 폴더 위치 지정
+            directory: true
+        }
+    });
+    gulp.watch("page/**/*.html").on("change", browserSync.reload);
+    // page 안의 파일들을 감시하고 있다가, 내용이 변동되면 재실행
 });
-gulp.watch("page/*").on("change", browserSync.reload);
-// page 안의 파일들을 감시하고 있다가, 내용이 변동되면 재실행
-});
-gulp.task( "default", gulp.parallel("browserSync") );
+gulp.task( "browser:watch", gulp.series("browserSync") );
 
 
-
-
-//sass
-
-const sass = require('gulp-sass')(require('sass'));
 
 // compile scss into css
 gulp.task('sass', function(){
-    return gulp.src('../css_dev/*.scss')
+    return gulp.src('../css_dev/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('../css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+        .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('watch', gulp.parallel('sass', 'browserSync'), function(){
-    gulp.watch('../css_dev/*.scss', gulp.parallel('sass'));
+// 파일 변경 감지
+gulp.task('sass:watch', function(){
+    gulp.watch('../css_dev/**/*.scss', gulp.series('sass'));
 });
 
 
-/* gulp.task(
+/* =========================================================================== */
+gulp.task(
     "default",
-    gulp.parallel("sass", "watch", "browserSync") // ★★★★ add browserSync
+    gulp.parallel("include:watch", "browser:watch" , "sass:watch")
 );
- */
+
 
