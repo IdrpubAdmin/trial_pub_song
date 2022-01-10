874b345753af0eabccc916a91a338c5bcc18ba3f
undo <undo@idrsystems.co.kr>
2022-01-10 18:02:48 +0900

    gulp setting complete

--- study/provence/dev/gulpfile.js
+++ study/provence/dev/gulpfile.js
@@ -3,9 +3,13 @@ const gulp = require('gulp');
 const fileinclude = require('gulp-file-include');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
+// const connect = require('gulp-connect');
+// const webserver = require('gulp-webserver');
 
 
-// html파일 인클루드
+/*===========================*/
+/*  html파일 인클루드         */
+/*===========================*/
 gulp.task('fileinclude', async function () {
 gulp.src(['**/*.html'])
     .pipe(fileinclude({
@@ -15,18 +19,38 @@ gulp.src(['**/*.html'])
     .pipe(gulp.dest('../dist'));
 });
 
-// 파일 변경 감지
+/*===========================*/
+/*  html파일 변경 감지        */
+/*===========================*/
 gulp.task('include:watch', function () {
     gulp.watch('**/*.html', gulp.series('fileinclude'));
     return
 })
 
+/*===========================*/
+/*  compile scss into css    */
+/*===========================*/
+gulp.task('sass', function(){
+    return gulp.src('../css_dev/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('../css')) //해당 task의 결과물이 저장될 경로 지정
+        .pipe(browserSync.reload({stream: true}));
+});
 
-//liveserver
+/*===========================*/
+/*  scss 파일 변경 감지       */
+/*===========================*/
+gulp.task('sass:watch', function(){
+    gulp.watch('../css_dev/**/*.scss', gulp.series('sass'));
+});
+
+/*===========================*/
+/*  liveserver               */
+/*===========================*/
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
-            baseDir: "../dist/page", // 서버에 띄울 폴더 위치 지정
+            baseDir: "../", // 서버에 띄울 폴더 위치 지정 (베이스디렉토리)
             directory: true
         }
     });
@@ -36,25 +60,47 @@ gulp.task('browserSync', function() {
 gulp.task( "browser:watch", gulp.series("browserSync") );
 
 
-
-// compile scss into css
-gulp.task('sass', function(){
-    return gulp.src('../css_dev/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('../css'))
-        .pipe(browserSync.reload({stream: true}));
-});
-
-// 파일 변경 감지
-gulp.task('sass:watch', function(){
-    gulp.watch('../css_dev/**/*.scss', gulp.series('sass'));
-});
-
-
-/* =========================================================================== */
+/*===========================*/
+/*  gulp watch               */
+/*===========================*/
 gulp.task(
     "default",
     gulp.parallel("include:watch", "browser:watch" , "sass:watch")
 );
 
 
+
+
+/* =========================================================================== */
+// gulp 서버
+// gulp.task('connect', function(){
+//     connect.server({
+//         root: '../',
+//         livereload: true,
+//         port: 8001
+//     });
+// });
+
+// gulp.task('html', function(){
+//     gulp.src('**/*.html')
+//     .pipe(connect.reload());
+// });
+
+// gulp.task('watch', function(){
+//     gulp.watch('**/*.html', gulp.series('fileinclude')); // html 리로드
+//     gulp.watch('../css_dev/**/*.scss', gulp.series('sass')); // sass 자동 컴파일
+// });
+
+// gulp.task('default', gulp.parallel('connect', 'watch'));
+
+// gulp 서버2
+// gulp.task('webserver', function(){
+//     gulp.src('../')
+//     .pipe(webserver({
+//         livereload: true,
+//         open: true,
+//         port: 8888
+//     }));
+// });
+// gulp.task('default2', gulp.parallel('webserver'));\
+/* =========================================================================== */
