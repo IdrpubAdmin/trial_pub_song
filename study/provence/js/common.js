0e32969eaa908ef53484ff812888b5180c0ff0c0
undo <undo@idrsystems.co.kr>
2022-01-26 09:05:18 +0900

    주차타워마크업 완료, 탭 js진행중

--- study/provence/js/common.js
+++ study/provence/js/common.js
@@ -6,9 +6,9 @@
 /*----------------------------------------------------------- */
 
 (function() {
-    'use strict';
+/*     'use strict'; */
 
-    // 메뉴 호버시 서브메뉴 드롭다운
+    /*------- menu hover dropdown -------*/
     document.addEventListener('DOMContentLoaded',  () => {
         const toggleBtns = document.querySelectorAll('.nav-menu>li');
 
@@ -17,7 +17,7 @@
             let navItem1d = navItem.querySelector('.nav-menu>li>a');
             let navItem2d = navItem.querySelector('.sub-menu');
 
-            // 1depth에 마우스 오버시 메뉴 노출
+            // 1depth mouse hover show
             navItem.addEventListener('mouseover', function () {
                 navItem2d.classList.add('active');
                 navItem1d.classList.add('on');
@@ -28,11 +28,32 @@
                 navItem1d.classList.remove('on');
             })
         });
-
     });
+    /*-------// menu hover dropdown -------*/
+
+    /*------ tower tab ------*/
+    const tabMenu = document.querySelectorAll('.content .tower_tab > li > a'),
+    tabContent = document.querySelectorAll('.content .tab_off');
+
+    for(i=0; i<tabMenu.length; i++){
+        tabMenu[i].addEventListener('click',function(e){
+            e.preventDefault();
+            for(x=0; x<tabMenu.length; x++){
+                tabMenu[x].classList.remove('on');
+            }
+            this.classList.add('on');
+            const target = this.getAttribute('href');
+
+            for(y=0; y<tabContent.length; y++){
+                tabContent[y].style.display = 'none';
+            }
+            document.querySelector(target).style.display='flex';
+        });
+    }
+    /* -----// tower tab -----*/
+})()
 
 
-})() /* //---end--- */
 
 
 
